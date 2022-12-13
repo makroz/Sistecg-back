71e59fa6b2de127557e2ee9a6b3c592afb5d09ea
Mario Guzman PC <makroz@hotmail.com>
2022-12-13 09:53:18 -0400

    hecho el crud

--- app/Http/Controllers/PersonaController.php
+++ app/Http/Controllers/PersonaController.php
@@ -7,80 +7,31 @@ use Illuminate\Http\Request;
 
 class PersonaController extends Controller
 {
-  /**
-   * Display a listing of the resource.
-   *
-   * @return \Illuminate\Http\Response
-   */
   public function index()
   {
     $lista = Persona::All();
     return response()->json($lista);
   }
 
-  /**
-   * Show the form for creating a new resource.
-   *
-   * @return \Illuminate\Http\Response
-   */
-  public function create()
-  {
-    //
-  }
-
-  /**
-   * Store a newly created resource in storage.
-   *
-   * @param  \Illuminate\Http\Request  $request
-   * @return \Illuminate\Http\Response
-   */
   public function store(Request $request)
   {
-    //
+    $persona = Persona::create($request->all());
+    return response()->json(['status' => 'ok', 'data' => $persona]);
   }
 
-  /**
-   * Display the specified resource.
-   *
-   * @param  \App\Models\Persona  $persona
-   * @return \Illuminate\Http\Response
-   */
-  public function show(Persona $persona)
+  public function show($id)
   {
-    //
+    $persona = Persona::find($id);
+    return response()->json(['status' => 'ok', 'data' => $persona]);
   }
 
-  /**
-   * Show the form for editing the specified resource.
-   *
-   * @param  \App\Models\Persona  $persona
-   * @return \Illuminate\Http\Response
-   */
-  public function edit(Persona $persona)
-  {
-    //
-  }
-
-  /**
-   * Update the specified resource in storage.
-   *
-   * @param  \Illuminate\Http\Request  $request
-   * @param  \App\Models\Persona  $persona
-   * @return \Illuminate\Http\Response
-   */
   public function update(Request $request, Persona $persona)
   {
-    //
+    $persona->update($request->all());
   }
 
-  /**
-   * Remove the specified resource from storage.
-   *
-   * @param  \App\Models\Persona  $persona
-   * @return \Illuminate\Http\Response
-   */
   public function destroy(Persona $persona)
   {
-    //
+    $persona->delete();
   }
 }
