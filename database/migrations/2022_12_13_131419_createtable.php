5f407ce94e34455189d9fe623cad2de5e1042c17
Mario Guzman PC <makroz@hotmail.com>
2022-12-13 09:40:52 -0400

    persona

--- database/migrations/2022_12_13_131419_createtable.php
+++ database/migrations/2022_12_13_131419_createtable.php
@@ -13,7 +13,7 @@ return new class extends Migration
    */
   public function up()
   {
-    Schema::create('persona', function (Blueprint $table) {
+    Schema::create('personas', function (Blueprint $table) {
       $table->id();
       $table->string('name');
       $table->date('age');
